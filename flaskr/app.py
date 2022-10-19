faf9bb6825e9de96d5340fe08b5237f0d0a6219c
Luis Guzman <luis.dev@lmaero.pro>
2022-10-18 23:37:45 -0500

    Update Docker instructions

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -22,3 +24,7 @@ cors = CORS(app)
 api = Api(app)
 
 jwt = JWTManager(app)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
