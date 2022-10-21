fe876ae81aa3b697b50208e1c1045b85a7388129
Alonso Cantu <a.cantu@uniandes.edu.co>
2022-10-21 00:08:15 -0700

    feature/3: Return tasks created

--- flaskr/app.py
+++ flaskr/app.py
@@ -7,6 +7,7 @@ from flask_restful import Api
 
 from modelos import db, Username
 from vistas.VistaLogin import VistaLogin
+from vistas.VistaTasks import VistaTasks
 
 app = Flask(__name__)
 
@@ -15,11 +16,11 @@ POSTGRES_PASS = os.environ.get("POSTGRES_PASSWORD")
 APP_DB_NAME = os.environ.get("APP_DB_NAME")
 
 # Docker URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://{}:{}@db:5432/{}'.format(POSTGRES_USER, POSTGRES_PASS,
-                                                                                      APP_DB_NAME)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://{}:{}@db:5432/{}'.format(POSTGRES_USER, POSTGRES_PASS,
+#                                                                                      APP_DB_NAME)
 
 # Local URL
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://admin:admin@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://admin:admin@localhost:5432/postgres'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
@@ -35,6 +36,8 @@ cors = CORS(app)
 
 api = Api(app)
 api.add_resource(VistaLogin, '/api/auth/login')
+api.add_resource(VistaTasks, '/api/tasks')
+
 
 jwt = JWTManager(app)
 
