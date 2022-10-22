31b621123d3c6b78387c45e130636d2829114b0b
Luis Guzman <luis.dev@lmaero.pro>
2022-10-22 15:31:03 -0500

    Modify PUT method for task

--- flaskr/vistas/VistaTask.py
+++ flaskr/vistas/VistaTask.py
@@ -1,18 +1,16 @@
-from cgi import test
 import glob
+import os
 from os import getcwd
-from pickle import EMPTY_DICT
-from queue import Empty
+
 from flask import request
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from modelos import File
-from pydub  import AudioSegment
 
-
-from modelos import db, Task, TaskSchema
+from modelos import db, Task, TaskSchema, File
 
 task_schema = TaskSchema()
+
+ALLOWED_EXTENSIONS = {'mp3', 'acc', 'ogg', 'wav', 'wma'}
 
 mp3_files = glob.glob('./*.mp3')
 acc_files = glob.glob('./*.acc')
@@ -20,7 +18,7 @@ ogg_files = glob.glob('./*.ogg')
 wav_files = glob.glob('./*.wav')
 wma_files = glob.glob('./*.wma')
 
-all_files=[mp3_files,acc_files,ogg_files,wav_files,wma_files]
+all_files = [mp3_files, acc_files, ogg_files, wav_files, wma_files]
 
 
 class VistaTask(Resource):
@@ -36,62 +34,30 @@ class VistaTask(Resource):
 
     @jwt_required()
     def put(self, id_task):
-        
-        
-        
-        
-        task = Task.query.get_or_404(id_task)
-        file_id=request.form.get("file",task.file)
-        print(request.form.get("file",task.file))
-        file_query = File.query.filter_by(id=file_id).first()
-        
-        print(file_query.filename)
-        original_file_url= getcwd()+"/files/"+file_query.filename+file_query.extension
-        print(request.form.get("new_format",task.new_format))
-        cadena=".mp3"
-        print(cadena.replace(".",""))
-
+        if "desired_format" not in request.form:
+            return {"message": "You should provide the new format"}, 400
+        if request.form["desired_format"] not in ALLOWED_EXTENSIONS:
+            return {"message": "That's not a valid extension"}, 400
 
         try:
-            if (request.form.get("status", task.status) == "PROCESSED") and ('new_format' in list(request.form.keys()) ):
-                task.new_format = request.form.get("new_format", task.new_format)
-                task.status = "UPLOADED"
-                # acá se borra el archivo anterior
-                db.session.commit()
-                return task_schema.dump(task)
+            task = Task.query.get_or_404(id_task)
+            file = db.session.query(File).filter(File.id == task.file).first()
+            processed_file_url = getcwd() + "/files/" + file.filename + "." + task.new_format
 
-            elif request.form.get("status", task.status) == "UPLOADED":
-                
-                if 'new_format' in list(request.form.keys()):         
-                      
-                            
-                    
-                    task.new_format = request.form.get("new_format", task.new_format)
-                    db.session.commit()
-                    return task_schema.dump(task)
-                else:
-                     
-                    task.status = "PROCESSED"
-                    original_format= request.form.get("original",task.original_format).replace(".","")
-                    new_format=request.form.get("new_format",task.new_format)
+            if task.status == "PROCESSED":
+                try:
+                    os.remove(processed_file_url)
+                except Exception as error:
+                    return {"message": error}
 
-                    new_file_url=getcwd()+"/files/"+file_query.filename+"."+new_format
-                    print(new_file_url)
+            task.new_format = request.form["desired_format"]
+            task.status = "UPLOADED"
+            db.session.add(task)
+            db.session.commit()
 
-                    new_audio=AudioSegment.from_file(original_file_url,format=original_format)
-                    new_audio.export(new_file_url,format=new_format)                    
-                    
-
-                    db.session.commit()
-                    return task_schema.dump(task)
-
-            else:
-                
-                return task_schema.dump(task)
-                
-
-        except:
-            return {"Message": "No task found"}
+            return task_schema.dump(task), 200
+        except Exception as error:
+            return {"message": error}
 
     @jwt_required()
     def delete(self, id_task):
