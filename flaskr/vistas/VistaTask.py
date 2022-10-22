83351135695741684d044adffe9aac518d4c07ac
diego.eslava <d.eslava@uniandes.edu.co>
2022-10-22 13:45:28 -0500

    Solved conflicts

--- flaskr/vistas/VistaTask.py
+++ flaskr/vistas/VistaTask.py
@@ -1,10 +1,26 @@
+from cgi import test
+import glob
+from os import getcwd
+from pickle import EMPTY_DICT
+from queue import Empty
 from flask import request
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
+from modelos import File
+from pydub  import AudioSegment
+
 
 from modelos import db, Task, TaskSchema
 
 task_schema = TaskSchema()
+
+mp3_files = glob.glob('./*.mp3')
+acc_files = glob.glob('./*.acc')
+ogg_files = glob.glob('./*.ogg')
+wav_files = glob.glob('./*.wav')
+wma_files = glob.glob('./*.wma')
+
+all_files=[mp3_files,acc_files,ogg_files,wav_files,wma_files]
 
 
 class VistaTask(Resource):
@@ -20,30 +36,59 @@ class VistaTask(Resource):
 
     @jwt_required()
     def put(self, id_task):
+        
+        
+        
+        
+        task = Task.query.get_or_404(id_task)
+        file_id=request.form.get("file",task.file)
+        print(request.form.get("file",task.file))
+        file_query = File.query.filter_by(id=file_id).first()
+        
+        print(file_query.filename)
+        original_file_url= getcwd()+"/files/"+file_query.filename+file_query.extension
+        print(request.form.get("new_format",task.new_format))
+        cadena=".mp3"
+        print(cadena.replace(".",""))
+
 
         try:
-            task = Task.query.get_or_404(id_task)
-
-            if task.status == "PROCESSED" and request.json.get("new_format", task.new_format):
-                task.new_format = request.json.get("new_format", task.new_format)
+            if (request.form.get("status", task.status) == "PROCESSED") and ('new_format' in list(request.form.keys()) ):
+                task.new_format = request.form.get("new_format", task.new_format)
                 task.status = "UPLOADED"
                 # acá se borra el archivo anterior
                 db.session.commit()
                 return task_schema.dump(task)
 
-            elif task.status == "UPLOADED":
-
-                if request.json.get("new_format", task.new_format):
-                    task.status = request.json.get("status", task.status)
+            elif request.form.get("status", task.status) == "UPLOADED":
+                
+                if 'new_format' in list(request.form.keys()):         
+                      
+                            
+                    
+                    task.new_format = request.form.get("new_format", task.new_format)
                     db.session.commit()
                     return task_schema.dump(task)
                 else:
-                    task.new_status = "PROCESSED"
+                     
+                    task.status = "PROCESSED"
+                    original_format= request.form.get("original",task.original_format).replace(".","")
+                    new_format=request.form.get("new_format",task.new_format)
+
+                    new_file_url=getcwd()+"/files/"+file_query.filename+"."+new_format
+                    print(new_file_url)
+
+                    new_audio=AudioSegment.from_file(original_file_url,format=original_format)
+                    new_audio.export(new_file_url,format=new_format)                    
+                    
+
                     db.session.commit()
                     return task_schema.dump(task)
 
             else:
-                return {"Message": "Unexpected error"}
+                
+                return task_schema.dump(task)
+                
 
         except:
             return {"Message": "No task found"}
