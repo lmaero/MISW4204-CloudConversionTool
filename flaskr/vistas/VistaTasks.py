57b84e7700b69d18c27d4cc0bc3e1aaedf6ced17
Luis Guzman <luis.dev@lmaero.pro>
2022-10-21 23:04:20 -0500

    Solve conflicts

--- flaskr/vistas/VistaTasks.py
+++ flaskr/vistas/VistaTasks.py
@@ -1,7 +1,12 @@
+import json
+import os
+
+from flask import request
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
-from modelos import Task, TaskSchema
+from app import app
+from modelos import Task, TaskSchema, db
 
 task_schema = TaskSchema()
 
@@ -16,3 +21,26 @@ class VistaTasks(Resource):
             return [task_schema.dump(task) for task in tasks]
         except:
             return {"message": "There is no information for tasks"}
+
+    @jwt_required()
+    def post(self):
+        file = request.files['file']
+        data = json.loads(request.data)
+        if 'file' not in request.files:
+            return {'message': 'No file part in the request'}, 400
+        if file.filename == '':
+            return {'message': 'No file selected for uploading'}, 400
+        if file and data:
+            path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            new_task = Task(
+                original_format=data["original_format"],
+                new_format=data["new_format"],
+                status="uploaded",
+                file=path
+            )
+            db.session.add(new_task)
+            db.session.commit()
+            file.save(path)
+            return TaskSchema.dump(new_task), 201
+        else:
+            return {'message': 'File not allowed'}, 400
