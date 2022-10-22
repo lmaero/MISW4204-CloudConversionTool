72b1b6f5b788b2d34176bae599459a661199c67b
Luis Guzman <luis.dev@lmaero.pro>
2022-10-21 19:01:33 -0500

    Add VistaFile.py endpoint

---
+++ flaskr/vistas/VistaFile.py
@@ -0,0 +1,23 @@
+from flask_jwt_extended import jwt_required
+from flask_restful import Resource
+
+from modelos import File, FileSchema
+
+file_schema = FileSchema()
+
+
+class VistaFile(Resource):
+
+    @jwt_required()
+    def get(self, filename):
+        try:
+            file_query = File.query.filter_by(filename=filename).first()
+            file = file_schema.dump(file_query)
+
+            if file:
+                return file, 200
+            else:
+                return {"message": "There is no filename with this name"}
+
+        except BaseException as error:
+            return {"message": error}
