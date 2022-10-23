986e4ba0c53cebf7e569facf6b86c19cce0ea638
Luis Guzman <luis.dev@lmaero.pro>
2022-10-22 20:29:17 -0500

    Improve GET file method

--- flaskr/vistas/VistaFile.py
+++ flaskr/vistas/VistaFile.py
@@ -1,4 +1,4 @@
-from flask import send_file
+from flask import send_file, request
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
@@ -11,12 +11,20 @@ class VistaFile(Resource):
 
     @jwt_required()
     def get(self, filename):
+        file_to_download = request.form["file_to_download"]
+
         try:
             file_query = File.query.filter_by(filename=filename).first()
             file = file_schema.dump(file_query)
 
+            original_file = "/app/files/" + file["filename"] + "." + file["task"][0]["original_format"]
+            processed_file = "/app/files/" + file["filename"] + "." + file["task"][0]["new_format"]
+
             if file:
-                return send_file(file["location"], as_attachment=True)
+                if file_to_download == "original":
+                    return send_file(original_file, as_attachment=True)
+                if file_to_download == "processed":
+                    return send_file(processed_file, as_attachment=True)
             else:
                 return {"message": "There is no filename with this name"}
 
