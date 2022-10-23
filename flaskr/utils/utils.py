1c96f3d37e038d1a03a5c91fb42857f284bd8bdb
Luis Guzman <luis.dev@lmaero.pro>
2022-10-23 12:46:58 -0500

    Fix password_checker method

--- flaskr/utils/utils.py
+++ flaskr/utils/utils.py
@@ -9,25 +9,31 @@ def password_check(passwd):
     if len(passwd) < 6:
         message = 'Password length should be at least 6'
         is_valid_password = False
+        return is_valid_password, message
 
     if len(passwd) > 20:
-        message = 'Length should be not be greater than 8'
+        message = 'Length should be not be greater than 20'
         is_valid_password = False
+        return is_valid_password, message
 
     if not any(char.isdigit() for char in passwd):
         message = 'Password should have at least one numeral'
         is_valid_password = False
+        return is_valid_password, message
 
     if not any(char.isupper() for char in passwd):
         message = 'Password should have at least one uppercase letter'
         is_valid_password = False
+        return is_valid_password, message
 
     if not any(char.islower() for char in passwd):
         message = 'Password should have at least one lowercase letter'
         is_valid_password = False
+        return is_valid_password, message
 
     if not any(char in special_sym for char in passwd):
         message = 'Password should have at least one of the symbols $@#'
         is_valid_password = False
+        return is_valid_password, message
 
     return is_valid_password, message
