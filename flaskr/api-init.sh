60bc23b9a3cca9a310262b4a8d7076fc7e53cd89
Luis Guzman <luis.dev@lmaero.pro>
2022-10-23 23:14:23 -0500

    Add nginx and gunicorn

---
+++ flaskr/api-init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y ffmpeg netcat
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
+gunicorn --bind 0.0.0.0:6000 --log-level=DEBUG app:app app.py
